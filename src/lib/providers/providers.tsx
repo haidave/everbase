86b9eb7aed1af7fa5063a5aeca27624e46d64376
Dave Nguyen <dave@haidave.com>
2024-06-29 23:03:07 +0200

    chore(providers): add providers wrapper

---
+++ src/lib/providers/providers.tsx
@@ -0,0 +1,12 @@
+import { ReactQueryProvider } from './react-query-provider'
+import { ThemeProvider } from './theme-provider'
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ReactQueryProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        {children}
+      </ThemeProvider>
+    </ReactQueryProvider>
+  )
+}

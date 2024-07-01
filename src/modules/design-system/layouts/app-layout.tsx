c6d20828700264f51cf13a66538eff32dd44c6e6
Dave Nguyen <dave@haidave.com>
2024-07-01 22:03:58 +0200

    chore: add lp-layout

--- src/modules/design-system/layouts/app-layout.tsx
+++ src/modules/design-system/layouts/app-layout.tsx
@@ -1,9 +1,6 @@
-import { Header } from '@/modules/design-system/components/header/header'
-
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
       {/* w-screen prevent layout shift with scrollbar */}
       <main className="w-screen">{children}</main>
     </>

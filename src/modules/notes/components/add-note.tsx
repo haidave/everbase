6341bae68881b8e07317d470e25ca4a184985ca5
Dave Nguyen <dave@haidave.com>
2024-07-22 23:59:39 +0200

    feat(add-note): use rich text editor

--- src/modules/notes/components/add-note.tsx
+++ src/modules/notes/components/add-note.tsx
@@ -5,7 +5,6 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { SquarePenIcon } from 'lucide-react'
 import { useForm } from 'react-hook-form'
-import TextareaAutosize from 'react-textarea-autosize'
 
 import { Button } from '@/modules/design-system/components/button'
 import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/modules/design-system/components/dialog'
@@ -15,6 +14,7 @@ import { addNote } from '@/modules/notes/lib/actions'
 
 import { QUERY_KEYS } from '../lib/const'
 import { noteSchema, type NoteSchemaType } from '../lib/validation'
+import { RichTextEditor } from './rich-text-editor'
 
 export function AddNote() {
   const queryClient = useQueryClient()
@@ -25,8 +25,7 @@ export function AddNote() {
     resolver: zodResolver(noteSchema),
   })
 
-  const { control, handleSubmit, watch, reset } = form
-  const content = watch('content')
+  const { control, handleSubmit, reset } = form
 
   const mutation = useMutation({
     mutationFn: addNote,
@@ -71,15 +70,6 @@ export function AddNote() {
     }
   }, [isOpen])
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if ((event.metaKey || event.ctrlKey) && event.key === 'Enter') {
-      event.preventDefault()
-      void handleSubmit(onSubmit)()
-    }
-  }
-
-  const showSaveButton = content && content.trim() !== ''
-
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <TooltipProvider>
@@ -100,57 +90,30 @@ export function AddNote() {
         </Tooltip>
       </TooltipProvider>
 
-      <DialogContent className="grid gap-2 sm:max-w-3xl">
+      <DialogContent className="grid gap-2 focus:outline-none sm:max-w-3xl">
         <DialogTitle className="sr-only">Create new note</DialogTitle>
         <DialogDescription className="sr-only">
           Click save to create a new note. Press escape to cancel.
         </DialogDescription>
         <Form {...form}>
-          <form onSubmit={handleSubmit(onSubmit)} className="relative max-h-[75svh] pb-14">
+          <form onSubmit={handleSubmit(onSubmit)} className="relative max-h-[75svh]">
             <FormField
               control={control}
               name="content"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <TextareaAutosize
-                      autoFocus
-                      placeholder="Type something..."
-                      minRows={4}
-                      className="textarea-scrollbar size-full max-h-[calc(75svh-4rem)] grow resize-none bg-base px-6 pt-3 font-mono leading-relaxed focus:outline-none"
-                      onKeyDown={handleKeyDown}
+                    <RichTextEditor
+                      isSaving={isSaving}
+                      handleOnSubmit={() => {
+                        void handleSubmit(onSubmit)()
+                      }}
                       {...field}
                     />
                   </FormControl>
                 </FormItem>
               )}
             />
-            <div className="absolute inset-x-0 bottom-0 flex items-center justify-end">
-              {(showSaveButton || isSaving) && (
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="shiny" type="submit" disabled={isSaving}>
-                        {isSaving ? 'Saving...' : 'Save'}
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="bottom">
-                      <p className="flex items-center gap-1.5">
-                        <div className="flex items-center gap-1">
-                          <kbd className="pointer-events-none flex h-[1.125rem] select-none items-center rounded border border-line bg-primary-hover px-1 font-sans font-medium">
-                            ⌘
-                          </kbd>
-                          <kbd className="pointer-events-none flex h-[1.125rem] select-none items-center rounded border border-line bg-primary-hover px-1 font-sans font-medium">
-                            Enter
-                          </kbd>
-                        </div>
-                        to save note
-                      </p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              )}
-            </div>
           </form>
         </Form>
       </DialogContent>

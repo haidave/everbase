c8e5bacfc4a83f03a9bd58b258503c3b022ba379
Dave Nguyen <dave@haidave.com>
2024-07-22 23:58:26 +0200

    chore(tailwind-config): add 3xs font size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,7 @@ module.exports = {
       },
       fontSize: {
         '2xs': '0.6875rem', // 11px
+        '3xs': '0.625rem', // 10px
       },
       textColor: {
         primary: 'hsl(var(--text-primary) / <alpha-value>)',

710d596a6db024e544f4b9ca5f3321110c49791a
Dave Nguyen <dave@haidave.com>
2024-07-26 23:25:14 +0200

    chore(next-config): add google avatars to image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     reactCompiler: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig

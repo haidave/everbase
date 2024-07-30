49ef39196c0fe30e0e284cf0eab81789c6f036bf
Dave Nguyen <dave@haidave.com>
2024-07-30 20:23:01 +0200

    feat(hero): quick responsive changes

--- src/modules/home/components/hero.tsx
+++ src/modules/home/components/hero.tsx
@@ -1,11 +1,11 @@
 const Hero = () => {
   return (
-    <section className="relative mx-auto mt-36 flex max-w-2xl flex-col items-center gap-6">
-      <h1 className="text-gradient text-center text-6xl font-medium leading-[1.2]">
-        Everything you need <br />
+    <section className="relative mx-auto mt-16 flex max-w-2xl flex-col items-center gap-6 md:mt-36">
+      <h1 className="text-gradient text-center text-hero-title font-medium leading-[1.2]">
+        Everything you&nbsp;need <br />
         in one place.
       </h1>
-      <p className="text-balance text-center text-lg leading-7 text-secondary">
+      <p className="text-balance text-center text-base leading-7 text-secondary md:text-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aliquid culpa eos veritatis rem aut optio
         suscipit, ratione porro at!
       </p>

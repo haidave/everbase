d3e6dc7c92eceeb732ffadc5f9a179b0847584bd
Dave Nguyen <dave@haidave.com>
2024-08-07 00:00:58 +0200

    fix(notes): adjust loading more position correctly

--- src/modules/notes/components/notes.tsx
+++ src/modules/notes/components/notes.tsx
@@ -70,70 +70,68 @@ const Notes = () => {
   }
 
   return (
-    <>
-      <div className="grid gap-8 pb-12 lg:gap-10">
-        {Object.entries(groupedNotes).map(([date, notes]) => (
-          <div key={date} className="relative">
-            <div className="flex items-center gap-2 font-mono text-sm lg:absolute lg:right-full lg:mr-10">
-              <span className="text-nowrap text-label">{formatDate(date)}</span>
-              <span className="text-secondary">{notes.length}</span>
-            </div>
-            <ul className="grid gap-4 max-lg:mt-3">
-              {notes.map((note) => (
-                <Popover
-                  key={note.id}
-                  open={openPopoverId === note.id}
-                  onOpenChange={(open) => setOpenPopoverId(open ? note.id : null)}
-                >
-                  <PopoverTrigger
-                    aria-label="Note actions"
-                    className="rounded-md text-left focus-visible:shadow-focus focus-visible:outline-0 [&[data-state='open']>li]:bg-primary"
-                  >
-                    <li className="relative rounded-md bg-subtle px-5 py-4 transition-all duration-150 hover:bg-primary active:bg-primary">
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html: note.content
-                            // Replace empty paragraphs with line breaks
-                            .replace(/<p><\/p>/g, '<br>')
-                            // Disable all inputs
-                            .replace(/<input/g, '<input tabindex="-1"'),
-                        }}
-                        className="pointer-events-none select-none whitespace-pre-wrap break-words font-mono text-sm leading-relaxed"
-                        style={{
-                          wordBreak: 'break-word',
-                        }}
-                        aria-readonly="true"
-                      ></div>
-                    </li>
-                  </PopoverTrigger>
-                  <PopoverContent
-                    ref={popoverContentRef}
-                    side={width < 1024 ? 'top' : 'right'}
-                    sideOffset={12}
-                    align={width < 1024 ? 'end' : 'start'}
-                    className="flex gap-1 rounded-md border border-line bg-subtle p-1"
-                    onOpenAutoFocus={(e) => {
-                      e.preventDefault()
-                      popoverContentRef.current?.setAttribute('tabindex', '-1')
-                      popoverContentRef.current?.focus()
-                    }}
-                  >
-                    <TooltipProvider delayDuration={300}>
-                      <EditNoteButton note={note} closePopover={closePopover} />
-                      <CopyNoteButton content={note.content} />
-                      <DeleteNoteButton noteId={note.id} />
-                    </TooltipProvider>
-                  </PopoverContent>
-                </Popover>
-              ))}
-            </ul>
+    <div className="relative grid gap-8 pb-12 lg:gap-10">
+      {Object.entries(groupedNotes).map(([date, notes]) => (
+        <div key={date} className="relative">
+          <div className="flex items-center gap-2 font-mono text-sm lg:absolute lg:right-full lg:mr-10">
+            <span className="text-nowrap text-label">{formatDate(date)}</span>
+            <span className="text-secondary">{notes.length}</span>
           </div>
-        ))}
-      </div>
+          <ul className="grid gap-4 max-lg:mt-3">
+            {notes.map((note) => (
+              <Popover
+                key={note.id}
+                open={openPopoverId === note.id}
+                onOpenChange={(open) => setOpenPopoverId(open ? note.id : null)}
+              >
+                <PopoverTrigger
+                  aria-label="Note actions"
+                  className="rounded-md text-left focus-visible:shadow-focus focus-visible:outline-0 [&[data-state='open']>li]:bg-primary"
+                >
+                  <li className="relative rounded-md bg-subtle px-5 py-4 transition-all duration-150 hover:bg-primary active:bg-primary">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: note.content
+                          // Replace empty paragraphs with line breaks
+                          .replace(/<p><\/p>/g, '<br>')
+                          // Disable all inputs
+                          .replace(/<input/g, '<input tabindex="-1"'),
+                      }}
+                      className="pointer-events-none select-none whitespace-pre-wrap break-words font-mono text-sm leading-relaxed"
+                      style={{
+                        wordBreak: 'break-word',
+                      }}
+                      aria-readonly="true"
+                    ></div>
+                  </li>
+                </PopoverTrigger>
+                <PopoverContent
+                  ref={popoverContentRef}
+                  side={width < 1024 ? 'top' : 'right'}
+                  sideOffset={12}
+                  align={width < 1024 ? 'end' : 'start'}
+                  className="flex gap-1 rounded-md border border-line bg-subtle p-1"
+                  onOpenAutoFocus={(e) => {
+                    e.preventDefault()
+                    popoverContentRef.current?.setAttribute('tabindex', '-1')
+                    popoverContentRef.current?.focus()
+                  }}
+                >
+                  <TooltipProvider delayDuration={300}>
+                    <EditNoteButton note={note} closePopover={closePopover} />
+                    <CopyNoteButton content={note.content} />
+                    <DeleteNoteButton noteId={note.id} />
+                  </TooltipProvider>
+                </PopoverContent>
+              </Popover>
+            ))}
+          </ul>
+        </div>
+      ))}
       <div ref={ref} className="absolute inset-x-0 bottom-0 grid h-10 place-items-center">
         {isFetchingNextPage ? <span className="font-mono text-sm text-secondary">Loading more...</span> : null}
       </div>
-    </>
+    </div>
   )
 }
 
